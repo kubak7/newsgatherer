2bf645dc61f1bf3aa370644d43b32ee0fe40deab
pbwojtowicz <33104143+pbwojtowicz@users.noreply.github.com>
2021-04-20 23:58:00 +0200

    Added routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home';
+import Register from '../views/Register';
+import Login from '../views/Login';
 import axios from 'vue-axios';
 import VueAxios from "vue-axios";
 
@@ -9,9 +11,19 @@ Vue.use(VueRouter);
 
 const routes = [
     {
-        path: '',
+        path: '/',
         name: 'Home',
         component: Home,
+    },
+    {
+        path: '/register',
+        name: 'Register',
+        component: Register,
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login,
     },
 ];
 
