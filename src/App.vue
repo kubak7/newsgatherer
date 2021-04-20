d24e7b35660d94b7575063bc92c1a3bea614d71e
pbwojtowicz <33104143+pbwojtowicz@users.noreply.github.com>
2021-04-20 23:57:03 +0200

    Added navigation

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link>|
+      <router-link to="/register">Register</router-link>|
+      <router-link to="/login">Login</router-link>
+    </div>
     <router-view></router-view>
   </div>
 </template>
