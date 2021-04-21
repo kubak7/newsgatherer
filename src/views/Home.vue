ca53cea51d56546618a1c17876bc47b4e042922e
100licki <57677238+100licki@users.noreply.github.com>
2021-04-21 18:11:11 +0200

    (need to fix css)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
-  <div class="root d-flex">
-    <router-link to="/">Home</router-link>|
-    <router-link to="/profil">Profil</router-link>|
-    <News></News>
-  </div>
+    <div class='root d-flex'>
+        <News></News>
+    </div>
 </template>
 
 <script>
-import News from "@/components/News";
+import News from '@/components/News';
+
 export default {
-  name: "Home",
-  components: {News}
-}
+    name: 'Home',
+    components: { News },
+};
 </script>
 
 <style scoped>
