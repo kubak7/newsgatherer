d31424963138b3d9c6a3887dcf9bebb98e351bb0
Patrycja Wójtowicz <pb.wojtowicz@gmail.com>
2021-04-22 11:50:54 +0200

    menaging firebase

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-app>
+      <News></News>
+    </v-app>
+</template>
+
+<script>
+
+
+export default {
+    name: 'Home',
+
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Register.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-app id="inspire">
+    <v-content>
+      <v-container fluid fill-height fill-width>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm12 md12 lg12 fill-height>
+            <v-card class="elevation-12">
+              <v-toolbar dark color="primary">
+                <v-toolbar-title>Create an account</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                      name="email"
+                      label="Email"
+                      type="text"
+                      v-model="email"
+                  ></v-text-field>
+                  <v-text-field
+                      id="password"
+                      name="password"
+                      label="Password"
+                      type="password"
+                      v-model="password"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" @click="register">Register</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
+
+</template>
+
+<script>
+import firebase from 'firebase';
+
+export default {
+    name: 'Register',
+    data() {
+        return {
+            email: '',
+            password: '',
+        };
+    },
+    methods: {
+        register() {
+            firebase
+                .auth()
+                .createUserWithEmailAndPassword(this.email, this.password)
+                .then(() => {
+                    alert('Successfully registered! Please login.');
+                    this.$router.push('/');
+                })
+                .catch(error => {
+                    alert(error.message);
+                });
+        },
+    },
+};
+</script>
